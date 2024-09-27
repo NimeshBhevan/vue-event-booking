46de29f104634d16cfccb64f8bb308c618da4e48
NimeshBhevan <nimeshbhevan.ar@gmail.com>
2024-09-27 12:58:35 +0530

    mock-json-server

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid grid-cols-2 gap-8" v-if="!eventLoading">
       <EventCard
-        v-for="i in 8"
-        :key="i"
-        title="hello"
-        when="now"
-        description="nothing"
+        v-for="event in events"
+        :key="event.id"
+        :title="event.title"
+        :when="event.date"
+        :description="event.description"
         @register="console.log('registered')"
       />
     </section>
+    <section v-else>Loading events....</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <BookingItem v-for="i in 3" :key="i"></BookingItem>
@@ -20,6 +21,24 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
 import BookingItem from './components/BookingItem.vue';
 import EventCard from './components/EventCard.vue';
+
+const events = ref([]);
+const eventLoading = ref(false);
+
+const fetchEvents = async () => {
+  eventLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    events.value = await response.json();
+  } finally {
+    eventLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchEvents();
+});
 </script>
